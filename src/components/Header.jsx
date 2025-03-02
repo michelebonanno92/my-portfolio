1d2dac93178511ac38cdab921f77fb9258fc68ea
micheleboanno92 <mikelebon7@gmail.com>
2025-03-02 19:37:42 +0100

    renovate nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,16 @@ export function Header() {
     <div>
       {/* Navbar */}
       <nav>
-        <a href="#home">Home</a>
-        <a href="#progetti">Progetti</a>
-        <a href="#contatti">Contatti</a>
+        <div>Portfolio</div>
+        <div>
+          <a href="#home">Su di me</a>
+          <a href="#progetti">Esperienze</a>
+          <a href="#progetti">Progetti</a>
+          <a href="#contatti">Contatti</a>
+        </div>
+    
       </nav>
 
-      {/* Sezioni */}
-      <section id="home" style={{ height: "100vh", backgroundColor: "lightblue" }}>
-        <h1>Home</h1>
-      </section>
-      <section id="progetti" style={{ height: "100vh", backgroundColor: "lightgreen" }}>
-        <h1>Progetti</h1>
-      </section>
-      <section id="contatti" style={{ height: "100vh", backgroundColor: "lightcoral" }}>
-        <h1>Contatti</h1>
-      </section>
     </div>
   );
 }
