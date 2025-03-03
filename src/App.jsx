9ffc65d0b4b9e1de17bcd2c1cd90976dc6bc85d5
micheleboanno92 <mikelebon7@gmail.com>
2025-03-03 16:24:59 +0100

    finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import './Header.css'
+import './Navbar.css'
+
 
 import { Header } from './components/Header'
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
       <Header/>  
+      
 
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,11 @@ export function Header() {
   return (
     <header>
       {/* Navbar */}
-      <nav>
-        {/* parte sinistra della navbar*/}
-        <div>Portfolio</div>
-        {/* parte destra della navbar */}
-        <div>
-          <a href="#Sudime">Su di me</a>
-          <a href="#progetti">Esperienze</a>
+      <nav className="navbar">
+          <a href="#chisono">Chi sono</a>
+          <a href="#esperienze">Esperienze</a>
           <a href="#progetti">Progetti</a>
           <a href="#contatti">Contatti</a>
-        </div>
       </nav>
 
     </header>
