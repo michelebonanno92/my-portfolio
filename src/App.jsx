71a729484109a6c68f1b03a7c35cc69698e74609
micheleboanno92 <mikelebon7@gmail.com>
2025-03-03 19:47:34 +0100

    SectionChiSono component created

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import './Header.css'
 import './Navbar.css'
 
 
+
 import { Header } from './components/Header'
+import { SectionChiSono } from "./components/SectionChiSono";
+
 
 function App() {
 
   return (
     <>
       <Header/>  
+      <SectionChiSono />
       
 
     </>
---
+++ src/components/SectionChiSono.jsx
@@ -0,0 +1,12 @@
+export function SectionChiSono() {
+  return (
+    <section id="chisono">
+    <h2>Chi Sono</h2>
+    <p>
+      Mi chiamo [Nome], sono uno sviluppatore appassionato di tecnologia e
+      innovazione. Amo creare soluzioni software che migliorano la vita
+      quotidiana.
+    </p>
+  </section>
+  );
+}
