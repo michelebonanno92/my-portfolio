5f3d757b0f09bf5c55b96721a54856ac6fe4aa8a
micheleboanno92 <mikelebon7@gmail.com>
2025-03-19 20:58:32 +0100

    fix contatti

--- src/components/SectionContatti.jsx
+++ src/components/SectionContatti.jsx
@@ -8,17 +8,21 @@ export function SectionContatti() {
         <section id='contatti' className="contatti-section">  
             <h2>Contatti</h2>    
             <div className="contact-info">  
-                <a href="mailto:mikelebon7@gmail.com" className="contact-item">  
+                <div className="contact-item">  
+                    <img src="path/to/email-icon.png" alt="Email" />
+                    <span>3884791600</span>  
+                </div>
+                <div className="contact-item">  
                     <img src="path/to/email-icon.png" alt="Email" />
                     <span>mikelebon7@gmail.com</span>  
-                </a>  
+                </div>  
                 <a href="https://www.linkedin.com/in/michele-bonanno-9886a01a3/" className="contact-item" target="_blank" rel="noopener noreferrer">  
                     <img src="path/to/linkedin-icon.png" alt="LinkedIn" />
-                    <span>linkedin.com/MicheleBonanno</span>  
+                    <span>Linkedin</span>  
                 </a>  
                 <a href="https://github.com/michelebonanno92" className="contact-item" target="_blank" rel="noopener noreferrer">  
                     <img src="path/to/github-icon.png" alt="GitHub" />
-                    <span>github.com/MicheleBonanno</span>  
+                    <span>GitHub</span>  
                 </a>  
             </div>  
         </section>  
