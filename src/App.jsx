56aa74eeefdbc5abfc0d06f3bb8dc21c93600b0d
micheleboanno92 <mikelebon7@gmail.com>
2025-03-19 21:14:30 +0100

    fix icons contatti

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,6 @@ function App() {
       <SectionEsperienze />
       <SectionProgetti />
       <SectionContatti />
-
-
     </>
   )
 }
--- src/components/SectionContatti.jsx
+++ src/components/SectionContatti.jsx
@@ -6,22 +6,22 @@ export function SectionContatti() {
 // export const SectionContatti = () => {
     return (  
         <section id='contatti' className="contatti-section">  
-            <h2>Contatti</h2>    
-            <div className="contact-info">  
+            <div className="contact-info"> 
+                <h2>Contatti</h2> 
                 <div className="contact-item">  
-                    <img src="path/to/email-icon.png" alt="Email" />
+                    <img src="https://img.icons8.com/color/48/phone.png" alt="Email" />
                     <span>3884791600</span>  
                 </div>
                 <div className="contact-item">  
-                    <img src="path/to/email-icon.png" alt="Email" />
+                    <img src="https://img.icons8.com/color/48/email.png"alt="Email" />
                     <span>mikelebon7@gmail.com</span>  
                 </div>  
                 <a href="https://www.linkedin.com/in/michele-bonanno-9886a01a3/" className="contact-item" target="_blank" rel="noopener noreferrer">  
-                    <img src="path/to/linkedin-icon.png" alt="LinkedIn" />
+                    <img src="https://img.icons8.com/color/48/linkedin.png" alt="LinkedIn" />
                     <span>Linkedin</span>  
                 </a>  
                 <a href="https://github.com/michelebonanno92" className="contact-item" target="_blank" rel="noopener noreferrer">  
-                    <img src="path/to/github-icon.png" alt="GitHub" />
+                    <img src="https://img.icons8.com/color/48/github.png" alt="GitHub" />
                     <span>GitHub</span>  
                 </a>  
             </div>  
